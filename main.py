007b8caf254ff3c26ba05aa74559ede1b5816621
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-04-20 03:40:36 +0000

    text and audio feature added

---
+++ main.py
@@ -0,0 +1,92 @@
+import logging
+import os
+import requests
+from dotenv import load_dotenv
+from telegram import (
+    Update,
+    ForceReply,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    ParseMode,
+)
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+    CallbackQueryHandler,
+)
+from utils import chatComplete, whisperModel
+
+load_dotenv()
+telegramToken = os.getenv("TELEGRAM_TOKEN")
+
+
+logger = logging.getLogger(__name__)
+
+
+def userMessage(update: Update, context: CallbackContext):
+    try:
+        if update.message.text:
+            name=update.message.chat.first_name
+            message = update.message.text.strip()
+            print(f"Message from {name}: {message}")
+            response = chatComplete(
+                message=message, name=name
+            )
+            print(f"Response: {response}")
+            update.message.reply_text(response)
+        # for voice messages
+        elif update.message.voice:
+            name = update.message.chat.first_name
+            voice = update.message.voice
+            voice_file = voice.get_file()
+            file_url = voice_file["file_path"]
+            file_name = file_url.split("/")[-1]  # Extract file name from URL
+            response = requests.get(file_url)
+            if response.status_code == 200:
+                with open(file_name, "wb") as f:
+                    f.write(response.content)
+                    whisperReply = whisperModel(audiofile=file_name,name=name)
+                    os.remove(file_name)
+                    print(f"Whisper Reply: {whisperReply}")
+                    update.message.reply_text(whisperReply)
+            else:
+                os.remove(file_name)
+                update.message.reply_text(
+                    "Something went wrong. Please try again later."
+                )
+        elif update.message.photo:
+            # print(f"photo is: {update.message}")
+            name = update.message.chat.first_name
+            data = update.message
+            image_urls = [photo['file_id'] for photo in data['photo']]
+            print(image_urls)
+
+        else:
+            print("Unsupported format.")
+            update.message.reply_text(
+                "I'm sorry, This format is not supported at the moment."
+            )
+    except Exception as e:
+        print(e)
+        update.message.reply_text("Something went wrong. Please try again later.")
+
+
+def main():
+    updater = Updater(telegramToken)
+    dispatcher = updater.dispatcher
+
+    # Register commands
+    dispatcher.add_handler(MessageHandler(Filters.all, userMessage))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,54 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+import base64
+
+load_dotenv()
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.getenv("OPENAI_KEY"),
+)
+
+
+def chatComplete(message: str, name: str):
+    try:
+        if not message:
+            return
+        else:
+
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    #  prompt
+                    {
+                        "role": "system",
+                        "content": f"You are a helpful telegram bot that has the access to the worldwide information. Your job is to help the users give them the answers to their questions. You will give the answer in concise way also keeping the user gets the quality answer they are looking for. So at the end the answers should be concise, impactful, easy to understand and, maintaining the quality is the top priority. If user ever passes an emoji your job would be to act humorous and give the answer in a fun way. You can also use the emojis in the answers. Try to be fun if the user is being sarcastic or fun. If the user is asking a question that is not in your knowledge you can ask the user to ask another question or you can say that you don't know the answer.Try to be fun and helpful. Make the user engage with you. Make the user like you and make the user feel that you are a friend. If the user asks a question which could have a funny ending give the user a humorous response. The user's name is {name} and use it in the conversation. Also, you have to remember the user's conversation done throughout the session.",
+                    },
+                    # message
+                    {"role": "user", "content": f"{message}"},
+                ],
+            )
+            return completion.choices[0].message.content
+    except Exception as e:
+        print(e)
+
+
+def whisperModel(audiofile, name):
+    try:
+        audio_file = open(audiofile, "rb")
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1", file=audio_file
+        )
+        voiceTranscription = transcript.text
+        print(f"Message from {name}: {voiceTranscription}")
+        return chatComplete(message=voiceTranscription, name=name)
+    except Exception as e:
+        return e
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
